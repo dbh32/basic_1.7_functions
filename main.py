c6eba0e408a21040d77f9348d722715b1153d528
dbh32 <51296575+dbh32@users.noreply.github.com>
2019-06-23 21:02:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ directories = {
 def search_name():
     '''Ищет человека по номеру документа'''
     number_input = input('Номер документа: ')
+
+    found = 0
     for doc_search in documents:
         if number_input in doc_search.values():
             print(doc_search['name'])
+            found += 1
+    if found == 0:
+        print('Документ с таким номером не обнаружен!')
 
 
 def show_all_docs():
@@ -34,9 +39,15 @@ def show_all_docs():
 def search_shelf():
     '''Ищет полку по номеру документа'''
     number_input = input('Номер документа: ')
+
+    found = 0
     for shelf, doc in directories.items():
         if number_input in doc:
             print(shelf)
+            found += 1
+
+    if found == 0:
+        print('Документ с таким номером не обнаружен!')
 
 
 def add_doc():
@@ -46,57 +57,82 @@ def add_doc():
     name_input = input('Кому принадлежит: ')
     shelf_input = input('Куда кладём: ')
 
-    new_doc = {
-        'type': type_input,
-        'number': number_input,
-        'name': name_input
-    }
-    documents.append(new_doc)
-
+    found = 0
     for shelf, doc in directories.items():
         if shelf_input in shelf:
             doc.append(number_input)
-        else:
-            pass
+            found += 1
+
+    if found > 0:
+        new_doc = {
+            'type': type_input,
+            'number': number_input,
+            'name': name_input
+        }
+        documents.append(new_doc)
+    else:
+        print('Такой полки не существует. Сначала создайте её командой "as".')
 
 
 def del_doc():
     '''Удаляет документ по номеру'''
     number_input = input('Номер документа: ')
+
+    found = 0
     for doc_search in documents:
         if number_input in doc_search.values():
             dict.clear(doc_search)
-    documents.remove({})
+            found =+ 1
+            documents.remove({})
+
+    if found > 0:
+        for doc in directories.values():
+            if number_input in doc:
+                doc.remove(number_input)
+    else:
+        print('Документ с таким номером не обнаружен!')
 
 
 def move_doc():
     '''Перемещает документ на выбранную полку'''
     number_input = input('Номер документа: ')
-    shelf_input = input('Куда перекладываем: ')
 
-    for shelf, doc in directories.items():
+    found = 0
+    for doc in directories.values():
         if number_input in doc:
             doc.remove(number_input)
-        else:
-            pass
+            found += 1
 
-        if shelf_input in shelf:
-            doc.append(number_input)
-        else:
-            pass
+    if found == 0:
+        print('Документ с таким номером не обнаружен!')
+        return TypeError
+    else:
+        shelf_input = input('Куда перекладываем: ')
+
+        for shelf, doc in directories.items():
+            if shelf_input in shelf:
+                doc.append(number_input)
+                found += 1
+
+        if found < 2:
+            print('Такой полки не существует. Сначала создайте её командой "as".')
 
 
 def create_shelf():
     '''Спрашивает номер новой полки и создаёт её в перечне'''
     new_shelf_input = input('Идентификатор новой полки: ')
 
+    found = 0
     dir_it = directories.copy()
     # Будем итерироваться по копии, чтобы поменять тело списка
     for shelf, doc in dir_it.items():
-        if new_shelf_input not in shelf:
-            directories[new_shelf_input] = []
-        else:
-            pass
+        if new_shelf_input in shelf:
+            found += 1
+
+    if found == 0:
+        directories[new_shelf_input] = []
+    else:
+        print('Такая полка уже существует!')
 
 
 def main():
